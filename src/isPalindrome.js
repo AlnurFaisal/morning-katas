117d61a1b8240c6c1e992b300dfd0a8b86c76f32
Alnur_Faisal <alnurfaisal@outlook.com>
2018-04-03 10:20:33 +0800

    added solution for isPalidrome and provided test cases

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -4,6 +4,13 @@ A palindrome is a word, phrase, number, or other sequence of characters which
 reads the same backward as forward, such as madam or racecar. 
 
 `input` will be a single string without spaces and punctuation. */
-const isPalindrome = input => {};
+const isPalindrome = input => {
+    let reverseStr = input.split("").reverse().join("");
+    if (reverseStr === input) {
+        return true;
+    } 
+    
+    return false;
+};
 
 module.exports = isPalindrome;
--- test/isPalindrome.test.js
+++ test/isPalindrome.test.js
@@ -1,9 +1,44 @@
 // Test cases: actual --> expected
 // isPalindrome('') --> true
+const isPalindrome = require("../src/isPalindrome");
+describe('is Palindrome', () => {
+    it("should return a blank as true", function() {   
+        expect(isPalindrome(" ")).toEqual(true);
+    })
+
+
 // isPalindrome('a') --> true
+    it("should return a character as true", function() {   
+        expect(isPalindrome("a")).toEqual(true);
+    })
+
 // isPalindrome('ab') --> false
+    it("should return two characters not a palindrome as false", function() {   
+        expect(isPalindrome("ab")).toEqual(false);
+    })
+
 // isPalindrome('abc') --> false
+it("should return three characters not a palindrome as false", function() {   
+    expect(isPalindrome("abc")).toEqual(false);
+})
 // isPalindrome('abba') --> true
+it("should return four characters that is a palindrome as true", function() {   
+    expect(isPalindrome("abba")).toEqual(true);
+})
+
 // isPalindrome('tacocat') --> true
+it("should return a word that is a palindrome as true", function() {   
+    expect(isPalindrome("tacocat")).toEqual(true);
+})
+
 // isPalindrome('racecar') --> true
+it("should return a word that is a palindrome as true", function() {   
+    expect(isPalindrome("racecar")).toEqual(true);
+})
+
 // isPalindrome('umbrella') --> false
+it("should return a word that is not a palindrome as false", function() {   
+    expect(isPalindrome("umbrella")).toEqual(false);
+})
+
+});
